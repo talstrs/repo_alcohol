5c99d2b070d76a368c6f89a16bee7153a6019eae
talstrs <tal95@daum.net>
2024-04-15 22:06:48 +0900

    리뷰 아작스 처리 진행

--- alcohol/src/main/java/com/babify/infra/review/ReviewController.java
+++ alcohol/src/main/java/com/babify/infra/review/ReviewController.java
@@ -1,8 +1,15 @@
 package com.babify.infra.review;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.babify.infra.product.ProductDto;
+import com.babify.infra.product.ProductService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -13,14 +20,34 @@ public class ReviewController {
 	@Autowired
 	ReviewService service;
 	
+	@Autowired
+	ProductService productService;
+	
+	
+	@ResponseBody
 	@RequestMapping(value = "/reviewInsert")
-	public String reviewInsert(ReviewDto dto, HttpSession httpSession) throws Exception{
+	public Map<String, Object> reviewInsert(ReviewDto dto, ProductDto pdto, HttpSession httpSession) throws Exception{
+		
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		
 		
 		dto.setReviewMembersSeq((String)httpSession.getAttribute("sessSeqUsr"));
 		
+		ProductDto dtoCheck = productService.selectOne(pdto);
+		
+
+		if(dtoCheck != null) {
+			
+			
+			service.insert(dto);
+			returnMap.put("rt", "success");
+		} else {
+			returnMap.put("rt", "fail");
+		}
+		
 		service.insert(dto);
 		
-		return "redirect:/productUsrList";
+		return returnMap;
 	}
 
 
