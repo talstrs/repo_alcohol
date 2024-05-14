60afff33a529993b0eaa1a8b0a251f02919e46a6
talstrs <tal95@daum.net>
2024-05-14 14:42:04 +0900

    파일업로디드 모듈 내용 추가

--- alcohol/src/main/java/com/babify/infra/fileuploaded/FileUploadedDao.java
+++ alcohol/src/main/java/com/babify/infra/fileuploaded/FileUploadedDao.java
@@ -1,6 +1,22 @@
 package com.babify.infra.fileuploaded;
 
+import java.util.List;
+
 public interface FileUploadedDao {
 	
-
+	public List<FileUploadedDto> selectList(FileUploadedVo vo);
+	
+	public FileUploadedDto selectOne(FileUploadedDto dto);
+	
+	public List<FileUploadedDto> selectListWithoutPaging();
+	
+	public int insert(FileUploadedDto dto);
+	
+	public int update(FileUploadedDto dto);
+	
+	public int updateDelete(FileUploadedDto dto);
+	
+	public int delete(FileUploadedDto dto);
+	
+	public int selectOneCount(FileUploadedVo vo);
 }
--- alcohol/src/main/java/com/babify/infra/fileuploaded/FileUploadedVo.java
+++ alcohol/src/main/java/com/babify/infra/fileuploaded/FileUploadedVo.java
@@ -1,10 +1,105 @@
 package com.babify.infra.fileuploaded;
 
+import java.util.Date;
+
 import com.babify.infra.BaseVo;
 
 public class FileUploadedVo extends BaseVo {
 		
-
+	private String fileUploadedSeq;
+	private Integer fileUploadedType;
+	private Integer fileUploadedDefaultNy;
+	private Integer fileUploadedSort;
+	private String fileUploadedPath;
+	private String fileUploadedOriginalName;
+	private String fileUploadedUuidName;
+	private String fileUploadedExt;
+	private Integer fileUploadedSize;
+	private Integer fileUploadedDelNy;
+	private String fileUploadedFseq;
+	private Date fileUploadedRegDateTime;
+	private Date fileUploadedModDateTime;
+	
+	public String getFileUploadedSeq() {
+		return fileUploadedSeq;
+	}
+	public void setFileUploadedSeq(String fileUploadedSeq) {
+		this.fileUploadedSeq = fileUploadedSeq;
+	}
+	public Integer getFileUploadedType() {
+		return fileUploadedType;
+	}
+	public void setFileUploadedType(Integer fileUploadedType) {
+		this.fileUploadedType = fileUploadedType;
+	}
+	public Integer getFileUploadedDefaultNy() {
+		return fileUploadedDefaultNy;
+	}
+	public void setFileUploadedDefaultNy(Integer fileUploadedDefaultNy) {
+		this.fileUploadedDefaultNy = fileUploadedDefaultNy;
+	}
+	public Integer getFileUploadedSort() {
+		return fileUploadedSort;
+	}
+	public void setFileUploadedSort(Integer fileUploadedSort) {
+		this.fileUploadedSort = fileUploadedSort;
+	}
+	public String getFileUploadedPath() {
+		return fileUploadedPath;
+	}
+	public void setFileUploadedPath(String fileUploadedPath) {
+		this.fileUploadedPath = fileUploadedPath;
+	}
+	public String getFileUploadedOriginalName() {
+		return fileUploadedOriginalName;
+	}
+	public void setFileUploadedOriginalName(String fileUploadedOriginalName) {
+		this.fileUploadedOriginalName = fileUploadedOriginalName;
+	}
+	public String getFileUploadedUuidName() {
+		return fileUploadedUuidName;
+	}
+	public void setFileUploadedUuidName(String fileUploadedUuidName) {
+		this.fileUploadedUuidName = fileUploadedUuidName;
+	}
+	public String getFileUploadedExt() {
+		return fileUploadedExt;
+	}
+	public void setFileUploadedExt(String fileUploadedExt) {
+		this.fileUploadedExt = fileUploadedExt;
+	}
+	public Integer getFileUploadedSize() {
+		return fileUploadedSize;
+	}
+	public void setFileUploadedSize(Integer fileUploadedSize) {
+		this.fileUploadedSize = fileUploadedSize;
+	}
+	public Integer getFileUploadedDelNy() {
+		return fileUploadedDelNy;
+	}
+	public void setFileUploadedDelNy(Integer fileUploadedDelNy) {
+		this.fileUploadedDelNy = fileUploadedDelNy;
+	}
+	public String getFileUploadedFseq() {
+		return fileUploadedFseq;
+	}
+	public void setFileUploadedFseq(String fileUploadedFseq) {
+		this.fileUploadedFseq = fileUploadedFseq;
+	}
+	public Date getFileUploadedRegDateTime() {
+		return fileUploadedRegDateTime;
+	}
+	public void setFileUploadedRegDateTime(Date fileUploadedRegDateTime) {
+		this.fileUploadedRegDateTime = fileUploadedRegDateTime;
+	}
+	public Date getFileUploadedModDateTime() {
+		return fileUploadedModDateTime;
+	}
+	public void setFileUploadedModDateTime(Date fileUploadedModDateTime) {
+		this.fileUploadedModDateTime = fileUploadedModDateTime;
+	}
+	
+	
 
 
 
