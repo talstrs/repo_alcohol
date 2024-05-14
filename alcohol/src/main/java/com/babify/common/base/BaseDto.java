491db6cadf6d0fcf29db7a9dc51fb090a12998b1
talstrs <tal95@daum.net>
2024-05-14 14:42:18 +0900

    baseDto 추가

---
+++ alcohol/src/main/java/com/babify/common/base/BaseDto.java
@@ -0,0 +1,22 @@
+package com.babify.common.base;
+
+public class BaseDto {
+	
+	private String seq;
+
+	public String getSeq() {
+		return seq;
+	}
+
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	
+	
+	
+}
+	
+	
+	
+	
+	
