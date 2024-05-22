a704b246401d967642890da874894a28121b2a61
talstrs <tal95@daum.net>
2024-05-22 12:50:59 +0900

    반려동물 동반 입장 가능 api 연동 및 usrindex 하단 노출

--- alcohol/src/main/java/com/babify/infra/product/ProductController.java
+++ alcohol/src/main/java/com/babify/infra/product/ProductController.java
@@ -1,5 +1,12 @@
 package com.babify.infra.product;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +17,12 @@ import com.babify.common.util.UtilSearch;
 import com.babify.infra.fileuploaded.FileUploadedDto;
 import com.babify.infra.fileuploaded.FileUploadedService;
 import com.babify.infra.fileuploaded.FileUploadedVo;
+import com.babify.infra.pet.PetDto;
 import com.babify.infra.review.ReviewDto;
 import com.babify.infra.review.ReviewService;
 import com.babify.infra.review.ReviewVo;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Controller
@@ -178,9 +188,52 @@ public String productUsrList(@ModelAttribute("vo") ProductVo vo, Model model) th
 public String usrIndex(ProductVo vo, Model model) throws Exception {
 	
     model.addAttribute("listBest", service.selectListUsrIndexBest(vo));
-    
     model.addAttribute("listNew", service.selectListUsrIndexNew(vo));
     
+    // api 관련
+    
+    String apiUrl = "https://www.pettravel.kr/api/listPart.do?page=1&pageBlock=16&partCode=PC01";
+	
+	URL url = new URL(apiUrl);
+	HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+	httpURLConnection.setRequestMethod("GET");
+	
+	BufferedReader bufferedReader;
+	if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+		bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+	} else {
+		bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+	}
+	
+	StringBuilder stringBuilder = new StringBuilder();
+	String line;
+	while ((line = bufferedReader.readLine()) != null) {
+		System.out.println("line: " + line);
+		stringBuilder.append(line);
+	}
+
+	bufferedReader.close();
+	httpURLConnection.disconnect();
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	JsonNode rootNode = objectMapper.readTree(stringBuilder.toString());
+	
+	// resultList에 해당하는 JsonNode 가져오기
+	JsonNode resultList = rootNode.get(0).get("resultList");
+	
+	List<PetDto> objectList = new ArrayList<>();
+	for (JsonNode node : resultList) {
+	    // JSON 데이터를 객체로 변환하여 리스트에 추가
+		PetDto obj = objectMapper.treeToValue(node, PetDto.class);
+	    objectList.add(obj);
+	}
+	
+	
+//	System.out.println("node.get(\"header\").get(\"resultList\").asText(): " + node.get("resultList").get(0).get("areaName").asText());
+	
+	model.addAttribute("node", objectList);
+	
+//	model.addAttribute(node);
     
     
     
